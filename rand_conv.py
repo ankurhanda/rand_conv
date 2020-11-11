d00bf332972892c94fbe17b7d7eee3a54f8a46e7
Ankur Handa <ahanda@nvidia.com>
2020-11-10 21:40:41 -0800

    adding readme, talking about fire
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- rand_conv.py
+++ rand_conv.py
@@ -12,7 +12,7 @@ def do_rand_conv(kernel_size=3,
                 alpha=0.7,
                 save_images=False):
     """
-    A function to apply random convolution filters to the image \
+    A function to apply random convolution filters to the image \\
     input (H x W x 3) ->rand_conv-> output (H x W x 3)
 
     @param: kernel_size is the size of the convolution filter to be used 
